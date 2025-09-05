9a477475cbbbf15826566e1b274b834b225a5eb7
danishahsancs <danishahsancs@gmail.com>
2025-09-05 10:50:42 -0400

    repository

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Vote.java
@@ -0,0 +1,34 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "VOTE_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "OPTION_ID")
+    private Option option;
+
+    // Getter and Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for option
+    public Option getOption() {
+        return option;
+    }
+
+    public void setOption(Option option) {
+        this.option = option;
+    }
+}
